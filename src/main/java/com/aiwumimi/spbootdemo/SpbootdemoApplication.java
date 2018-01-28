964c6380560107ae6b01fd851fd563fe7c1f7c51
anyongji <anyongjitiger@gmail.com>
2018-01-28 23:28:41 +0800

    第一次提交

---
+++ src/main/java/com/aiwumimi/spbootdemo/SpbootdemoApplication.java
@@ -0,0 +1,12 @@
+package com.aiwumimi.spbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbootdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpbootdemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/aiwumimi/spbootdemo/SpbootdemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.aiwumimi.spbootdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpbootdemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
